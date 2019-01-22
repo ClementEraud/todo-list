ecba671815a22b0b1b32b3d48e44cbad493aaeb9
ceraud <clement.eraud@capgemini.com>
2019-01-22 15:32:46 +0100

    add vuetify

---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="pure-g">
+        <div class="row">
+            <div class="col-sm">
+                item1
+            </div>
+        </div>
+        <div class="row">
+            item2
+        </div>
+        <div class="row">
+            item3
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TodoList'
+    }
+</script>
+
+<style>
+</style>
